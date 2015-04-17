1ab7d71ca184c717d548628c1635c16baa0f3cad
Fr0sZ <victormoller.95@gmail.com>
2015-04-18 00:25:15 +0200

    Added "Find in SceneExplorer" button for vehicles

--- Debugger/GamePanelExtender.cs
+++ Debugger/GamePanelExtender.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using ColossalFramework.UI;
 using UnityEngine;
+using UnityExtension;
 
 namespace ModTools
 {
 
     class GamePanelExtender : MonoBehaviour
     {
+
 
         private bool initializedBuildingsPanel = false;
         private ZonedBuildingWorldInfoPanel zonedBuildingInfoPanel;
@@ -19,6 +23,24 @@ namespace ModTools
         private SceneExplorer sceneExplorer;
 
         private ReferenceChain buildingsBufferRefChain;
+        private ReferenceChain vehiclesBufferRefChain;
+        private ReferenceChain vehiclesParkedBufferRefChain;
+
+        private bool initializedCitizenVehiclePanel = false;
+        private CitizenVehicleWorldInfoPanel citizenVehicleInfoPanel;
+        private UILabel citizenVehicleAssetNameLabel;
+        private UIButton citizenVehicleShowExplorerButton;
+
+        private bool initializedCityServiceVehiclePanel = false;
+        private CityServiceVehicleWorldInfoPanel cityServiceVehicleInfoPanel;
+        private UILabel cityServiceVehicleAssetNameLabel;
+        private UIButton cityServiceVehicleShowExplorerButton;
+
+        private bool initializedPublicTransportVehiclePanel = false;
+        private PublicTransportVehicleWorldInfoPanel publicTransportVehicleInfoPanel;
+        private UILabel publicTransportVehicleAssetNameLabel;
+        private UIButton publicTransportVehicleShowExplorerButton;
+
 
         void Awake()
         {
@@ -35,6 +57,21 @@ namespace ModTools
 
                 zonedBuildingInfoPanel.component.Find<UILabel>("AllGood").isVisible = true;
                 zonedBuildingInfoPanel.component.Find<UIPanel>("ProblemsPanel").isVisible = true;
+
+                Destroy(citizenVehicleAssetNameLabel.gameObject);
+                Destroy(citizenVehicleShowExplorerButton.gameObject);
+
+                citizenVehicleInfoPanel.component.Find<UILabel>("Type").isVisible = true;
+
+                Destroy(cityServiceVehicleAssetNameLabel.gameObject);
+                Destroy(cityServiceVehicleShowExplorerButton.gameObject);
+
+                cityServiceVehicleInfoPanel.component.Find<UILabel>("Type").isVisible = true;
+
+                Destroy(publicTransportVehicleAssetNameLabel.gameObject);
+                Destroy(publicTransportVehicleShowExplorerButton.gameObject);
+
+                publicTransportVehicleInfoPanel.component.Find<UILabel>("Type").isVisible = true;
             }
             catch (Exception)
             {
@@ -131,6 +168,42 @@ namespace ModTools
             );
         }
 
+        void AddVehiclePanelControls(WorldInfoPanel infoPanel, out UILabel assetNameLabel, out UIButton ShowExplorerButton)
+        {
+            infoPanel.component.Find<UILabel>("Type").isVisible = false;
+
+            assetNameLabel = CreateLabel
+            (
+                "AssetName: <>", 160, 24,
+                infoPanel.component,
+                new Vector3(8.0f, 48.0f, 0.0f),
+                UIAlignAnchor.TopLeft
+            );
+
+            ShowExplorerButton = CreateButton
+            (
+                "Find in SceneExplorer", 160, 24,
+                infoPanel.component,
+                new Vector3(-8.0f, -25.0f, 0.0f),
+                UIAlignAnchor.BottomRight,
+                (component, param) =>
+                {
+                    InstanceID instance = Util.GetPrivate<InstanceID>(infoPanel, "m_InstanceID");
+
+                    if (instance.Vehicle == 0)
+                    {
+                        sceneExplorer.ExpandFromRefChain(vehiclesParkedBufferRefChain.Add(instance.ParkedVehicle));
+                    }
+                    else
+                    {
+                        sceneExplorer.ExpandFromRefChain(vehiclesBufferRefChain.Add(instance.Vehicle));
+                    }
+
+                    sceneExplorer.visible = true;
+                }
+            );
+        }
+
         void Update()
         {
             if (!initializedBuildingsPanel)
@@ -151,14 +224,117 @@ namespace ModTools
                 }
             }
 
+            if (!initializedCitizenVehiclePanel)
+            {
+                sceneExplorer = FindObjectOfType<SceneExplorer>();
+
+                vehiclesBufferRefChain = new ReferenceChain();
+                vehiclesBufferRefChain = vehiclesBufferRefChain.Add(VehicleManager.instance.gameObject);
+                vehiclesBufferRefChain = vehiclesBufferRefChain.Add(VehicleManager.instance);
+                vehiclesBufferRefChain = vehiclesBufferRefChain.Add(typeof(VehicleManager).GetField("m_vehicles"));
+                vehiclesBufferRefChain = vehiclesBufferRefChain.Add(typeof(Array16<Vehicle>).GetField("m_buffer"));
+
+                vehiclesParkedBufferRefChain = new ReferenceChain();
+                vehiclesParkedBufferRefChain = vehiclesParkedBufferRefChain.Add(VehicleManager.instance.gameObject);
+                vehiclesParkedBufferRefChain = vehiclesParkedBufferRefChain.Add(VehicleManager.instance);
+                vehiclesParkedBufferRefChain = vehiclesParkedBufferRefChain.Add(typeof(VehicleManager).GetField("m_parkedVehicles"));
+                vehiclesParkedBufferRefChain = vehiclesParkedBufferRefChain.Add(typeof(Array16<VehicleParked>).GetField("m_buffer"));
+                
+                citizenVehicleInfoPanel = GameObject.Find("(Library) CitizenVehicleWorldInfoPanel").GetComponent<CitizenVehicleWorldInfoPanel>();
+                if (citizenVehicleInfoPanel != null)
+                {
+                    AddVehiclePanelControls(
+                        citizenVehicleInfoPanel, 
+                        out citizenVehicleAssetNameLabel, 
+                        out citizenVehicleShowExplorerButton);
+                    initializedCitizenVehiclePanel = true;
+                }
+            }
+
+            if (!initializedCityServiceVehiclePanel)
+            {
+                sceneExplorer = FindObjectOfType<SceneExplorer>();
+
+                cityServiceVehicleInfoPanel = GameObject.Find("(Library) CityServiceVehicleWorldInfoPanel").GetComponent<CityServiceVehicleWorldInfoPanel>();
+                if (cityServiceVehicleInfoPanel != null)
+                {
+                    AddVehiclePanelControls(
+                        cityServiceVehicleInfoPanel,
+                        out cityServiceVehicleAssetNameLabel,
+                        out cityServiceVehicleShowExplorerButton);
+                    initializedCityServiceVehiclePanel = true;
+                }
+            }
+            
+            if (!initializedPublicTransportVehiclePanel)
+            {
+                sceneExplorer = FindObjectOfType<SceneExplorer>();
+
+                publicTransportVehicleInfoPanel = GameObject.Find("(Library) PublicTransportVehicleWorldInfoPanel").GetComponent<PublicTransportVehicleWorldInfoPanel>();
+                if (publicTransportVehicleInfoPanel != null)
+                {
+                    AddVehiclePanelControls(
+                        publicTransportVehicleInfoPanel, 
+                        out publicTransportVehicleAssetNameLabel, 
+                        out publicTransportVehicleShowExplorerButton);
+                    initializedPublicTransportVehiclePanel = true;
+                }
+            }
+
             if (zonedBuildingInfoPanel.component.isVisible)
             {
                 InstanceID instance = Util.GetPrivate<InstanceID>(zonedBuildingInfoPanel, "m_InstanceID");
                 var building = BuildingManager.instance.m_buildings.m_buffer[instance.Building];
                 zonedBuildingAssetNameLabel.text = "AssetName: " + building.Info.name;
             }
+            
+            if (citizenVehicleInfoPanel.component.isVisible)
+            {
+                InstanceID instance = Util.GetPrivate<InstanceID>(citizenVehicleInfoPanel, "m_InstanceID");
+
+                if (instance.Vehicle == 0)
+                {
+                    var vehicle = VehicleManager.instance.m_parkedVehicles.m_buffer[instance.ParkedVehicle];
+                    citizenVehicleAssetNameLabel.text = "AssetName: " + vehicle.Info.name;
+                }
+                else
+                {
+                    var vehicle = VehicleManager.instance.m_vehicles.m_buffer[instance.Vehicle];
+                    citizenVehicleAssetNameLabel.text = "AssetName: " + vehicle.Info.name;
+                }
+            }
+
+            if (cityServiceVehicleInfoPanel.component.isVisible)
+            {
+                InstanceID instance = Util.GetPrivate<InstanceID>(cityServiceVehicleInfoPanel, "m_InstanceID");
+
+                if (instance.Vehicle == 0)
+                {
+                    var vehicle = VehicleManager.instance.m_parkedVehicles.m_buffer[instance.ParkedVehicle];
+                    cityServiceVehicleAssetNameLabel.text = "AssetName: " + vehicle.Info.name;
+                }
+                else
+                {
+                    var vehicle = VehicleManager.instance.m_vehicles.m_buffer[instance.Vehicle];
+                    cityServiceVehicleAssetNameLabel.text = "AssetName: " + vehicle.Info.name;
+                }
+            }
+
+            if (publicTransportVehicleInfoPanel.component.isVisible)
+            {
+                InstanceID instance = Util.GetPrivate<InstanceID>(publicTransportVehicleInfoPanel, "m_InstanceID");
+
+                if (instance.Vehicle == 0)
+                {
+                    var vehicle = VehicleManager.instance.m_parkedVehicles.m_buffer[instance.ParkedVehicle];
+                    publicTransportVehicleAssetNameLabel.text = "AssetName: " + vehicle.Info.name;
+                }
+                else
+                {
+                    var vehicle = VehicleManager.instance.m_vehicles.m_buffer[instance.Vehicle];
+                    publicTransportVehicleAssetNameLabel.text = "AssetName: " + vehicle.Info.name;
+                }
+            }
         }
-
     }
-
 }
