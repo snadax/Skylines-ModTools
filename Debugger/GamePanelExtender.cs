c50e8a8688551ac75893e53ef2ac41eed18bca8f
Egor Aralov <aei.omsk@gmail.com>
2016-06-12 03:29:24 +0600

    Fix citizen handling. Add find citizen, find instance and find unit buttons for all kinds of citizens

--- Debugger/GamePanelExtender.cs
+++ Debugger/GamePanelExtender.cs
@@ -31,6 +31,7 @@ namespace ModTools
         private ReferenceChain vehiclesParkedBufferRefChain;
         private ReferenceChain citizenInstancesBufferRefChain;
         private ReferenceChain citizensBufferRefChain;
+        private ReferenceChain citizensUnitsBufferRefChain;
 
         private bool initializedCitizenVehiclePanel = false;
         private CitizenVehicleWorldInfoPanel citizenVehicleInfoPanel;
@@ -55,13 +56,15 @@ namespace ModTools
         private AnimalWorldInfoPanel animalInfoPanel;
         private UILabel animalAssetNameLabel;
         private UIButton animalShowExplorerButton;
-        private UIButton animalDumpTextureMeshButton;
+        private UIButton animalShowInstanceButton;
+        private UIButton animalShowUnitButton;
 
         private bool initializedCitizenPanel = false;
         private HumanWorldInfoPanel citizenInfoPanel;
         private UILabel citizenAssetNameLabel;
         private UIButton citizenShowExplorerButton;
-        private UIButton citizenDumpTextureMeshButton;
+        private UIButton citizenShowInstanceButton;
+        private UIButton citizenShowUnitButton;
 
         void OnDestroy()
         {
@@ -102,7 +105,7 @@ namespace ModTools
 
                 Destroy(animalAssetNameLabel.gameObject);
                 Destroy(animalShowExplorerButton.gameObject);
-                Destroy(animalDumpTextureMeshButton.gameObject);
+                Destroy(animalShowInstanceButton.gameObject);
 
                 publicTransportVehicleInfoPanel.component.Find<UILabel>("Type").isVisible = true;
             }
@@ -247,7 +250,9 @@ namespace ModTools
 
         void AddCitizenPanelControls(WorldInfoPanel infoPanel, out UILabel assetNameLabel,
                 out UIButton showExplorerButton, Vector3 showExplorerButtonOffset,
-                out UIButton dumpMeshTextureButton, Vector3 dumpMeshTextureButtonOffset)
+                out UIButton showInstanceButton, Vector3 showInstanceButtonOffset,
+                out UIButton showUnitButton, Vector3 showUniteButtonOffset
+                )
         {
 
             assetNameLabel = CreateLabel
@@ -260,7 +265,7 @@ namespace ModTools
 
             showExplorerButton = CreateButton
             (
-                "Find in SceneExplorer", 160, 24,
+                "Find citizen in SE", 160, 24,
                 infoPanel.component,
                 showExplorerButtonOffset,
                 UIAlignAnchor.TopRight,
@@ -269,7 +274,12 @@ namespace ModTools
                     InstanceID instance = ReflectionUtil.GetPrivate<InstanceID>(infoPanel, "m_InstanceID");
                     if (instance.Type == InstanceType.CitizenInstance)
                     {
-                        sceneExplorer.ExpandFromRefChain(citizenInstancesBufferRefChain.Add(instance.CitizenInstance));
+                        var ci = CitizenManager.instance.m_instances.m_buffer[instance.CitizenInstance];
+                        if (ci.m_citizen == 0)
+                        {
+                            return;
+                        }
+                        sceneExplorer.ExpandFromRefChain(citizenInstancesBufferRefChain.Add((int)ci.m_citizen));
                     }
                     else if (instance.Type == InstanceType.Citizen)
                     {
@@ -279,20 +289,134 @@ namespace ModTools
                 }
             );
 
-            dumpMeshTextureButton = CreateButton
+            showInstanceButton = CreateButton
             (
-                "Dump asset", 160, 24,
+                "Find instance in SE   ", 160, 24,
                 infoPanel.component,
-                dumpMeshTextureButtonOffset,
+                showInstanceButtonOffset,
                 UIAlignAnchor.TopRight,
                 (component, param) =>
                 {
                     var instance = ReflectionUtil.GetPrivate<InstanceID>(infoPanel, "m_InstanceID");
                     if (instance.Type == InstanceType.CitizenInstance)
                     {
-                        var citizen = CitizenManager.instance.m_instances.m_buffer[instance.CitizenInstance];
-                        var assetName = citizen.Info.name;
-                        DumpUtil.DumpAsset(assetName, null, null, citizen.Info.m_lodMesh, citizen.Info.m_lodMaterial);
+                        sceneExplorer.ExpandFromRefChain(citizenInstancesBufferRefChain.Add(instance.CitizenInstance));
+                        sceneExplorer.visible = true;
+                    }
+                    else if (instance.Type == InstanceType.Citizen)
+                    {
+                        for (var index = 0; index < CitizenManager.instance.m_instances.m_buffer.Length; index++)
+                        {
+                            var ci = CitizenManager.instance.m_instances.m_buffer[index];
+                            if (ci.m_flags == CitizenInstance.Flags.None || ci.Info == null || ci.m_citizen != instance.Citizen)
+                            {
+                                continue;
+                            }
+                            sceneExplorer.ExpandFromRefChain(citizenInstancesBufferRefChain.Add(index));
+                            sceneExplorer.visible = true;
+                            break;
+                        }
+                    }
+                }
+            );
+
+            showUnitButton = CreateButton
+            (
+                "Find unit in SE   ", 160, 24,
+                infoPanel.component,
+                showUniteButtonOffset,
+                UIAlignAnchor.TopRight,
+                (component, param) =>
+                {
+                    var instance = ReflectionUtil.GetPrivate<InstanceID>(infoPanel, "m_InstanceID");
+                    if (instance.Type == InstanceType.CitizenInstance)
+                    {
+                        var ci = CitizenManager.instance.m_instances.m_buffer[instance.CitizenInstance];
+                        var citizen = ci.m_citizen;
+                        if (citizen == 0)
+                        {
+                            return;
+                        }
+                        for (var index = 0; index < CitizenManager.instance.m_units.m_buffer.Length; index++)
+                        {
+                            var cu = CitizenManager.instance.m_units.m_buffer[index];
+                            if (cu.m_flags == CitizenUnit.Flags.None)
+                            {
+                                continue;
+                            }
+                            uint unit = 0;
+                            if (cu.m_citizen0 == citizen)
+                            {
+                                unit = cu.m_citizen0;
+                            }
+                            else if (cu.m_citizen1 == citizen)
+                            {
+                                unit = cu.m_citizen1;
+                            }
+                            else if (cu.m_citizen2 == citizen)
+                            {
+                                unit = cu.m_citizen2;
+                            }
+                            else if (cu.m_citizen3 == citizen)
+                            {
+                                unit = cu.m_citizen3;
+                            }
+                            else if (cu.m_citizen4 == citizen)
+                            {
+                                unit = cu.m_citizen4;
+                            }
+                            if (unit == 0)
+                            {
+                                continue;
+                            }
+                            sceneExplorer.ExpandFromRefChain(citizensUnitsBufferRefChain.Add(index));
+                            sceneExplorer.visible = true;
+                            break;
+                        }
+                        sceneExplorer.visible = true;
+                    }
+                    else if (instance.Type == InstanceType.Citizen)
+                    {
+                        if (instance.Citizen == 0)
+                        {
+                            return;
+                        }
+                        for (var index = 0; index < CitizenManager.instance.m_units.m_buffer.Length; index++)
+                        {
+                            var cu = CitizenManager.instance.m_units.m_buffer[index];
+                            if (cu.m_flags == CitizenUnit.Flags.None)
+                            {
+                                continue;
+                            }
+                            uint unit = 0;
+                            if (cu.m_citizen0 == instance.Citizen)
+                            {
+                                unit = cu.m_citizen0;
+                            }
+                            else if (cu.m_citizen1 == instance.Citizen)
+                            {
+                                unit = cu.m_citizen1;
+                            }
+                            else if (cu.m_citizen2 == instance.Citizen)
+                            {
+                                unit = cu.m_citizen2;
+                            }
+                            else if (cu.m_citizen3 == instance.Citizen)
+                            {
+                                unit = cu.m_citizen3;
+                            }
+                            else if (cu.m_citizen4 == instance.Citizen)
+                            {
+                                unit = cu.m_citizen4;
+                            }
+                            if (unit == 0)
+                            {
+                                continue;
+                            }
+                            sceneExplorer.ExpandFromRefChain(citizensUnitsBufferRefChain.Add(index));
+                            sceneExplorer.visible = true;
+                            break;
+                        }
                     }
                 }
             );
@@ -416,14 +540,25 @@ namespace ModTools
                     .Add(CitizenManager.instance)
                     .Add(typeof(CitizenManager).GetField("m_instances"))
                     .Add(typeof(Array16<CitizenInstance>).GetField("m_buffer"));
+                citizensBufferRefChain = new ReferenceChain()
+                    .Add(CitizenManager.instance.gameObject)
+                    .Add(CitizenManager.instance)
+                    .Add(typeof(CitizenManager).GetField("m_citizens"))
+                    .Add(typeof(Array32<Citizen>).GetField("m_buffer"));
+                citizensUnitsBufferRefChain = new ReferenceChain()
+                    .Add(CitizenManager.instance.gameObject)
+                    .Add(CitizenManager.instance)
+                    .Add(typeof(CitizenManager).GetField("m_units"))
+                    .Add(typeof(Array32<CitizenUnit>).GetField("m_buffer"));
 
                 sceneExplorer = FindObjectOfType<SceneExplorer>();
                 animalInfoPanel = GameObject.Find("(Library) AnimalWorldInfoPanel").GetComponent<AnimalWorldInfoPanel>();
                 if (animalInfoPanel != null)
                 {
                     AddCitizenPanelControls(animalInfoPanel, out animalAssetNameLabel,
-                        out animalShowExplorerButton, new Vector3(-8.0f, 50.0f, 0.0f),
-                        out animalDumpTextureMeshButton, new Vector3(-8.0f, 75.0f, 0.0f)
+                        out animalShowExplorerButton, new Vector3(-8.0f, 65.0f, 0.0f),
+                        out animalShowInstanceButton, new Vector3(-8.0f, 90.0f, 0.0f),
+                        out animalShowUnitButton, new Vector3(-8.0f, 115.0f, 0.0f)
                         );
                     initializedAnimalPanel = true;
                 }
@@ -440,7 +575,12 @@ namespace ModTools
                     .Add(CitizenManager.instance.gameObject)
                     .Add(CitizenManager.instance)
                     .Add(typeof(CitizenManager).GetField("m_citizens"))
-                    .Add(typeof(Array16<CitizenInstance>).GetField("m_buffer"));
+                    .Add(typeof(Array32<Citizen>).GetField("m_buffer"));
+                citizensUnitsBufferRefChain = new ReferenceChain()
+                    .Add(CitizenManager.instance.gameObject)
+                    .Add(CitizenManager.instance)
+                    .Add(typeof(CitizenManager).GetField("m_units"))
+                    .Add(typeof(Array32<CitizenUnit>).GetField("m_buffer"));
 
 
                 sceneExplorer = FindObjectOfType<SceneExplorer>();
@@ -449,7 +589,8 @@ namespace ModTools
                 {
                     AddCitizenPanelControls(citizenInfoPanel, out citizenAssetNameLabel,
                         out citizenShowExplorerButton, new Vector3(-8.0f, 110.0f, 0.0f),
-                        out citizenDumpTextureMeshButton, new Vector3(-8.0f, 135.0f, 0.0f)
+                        out citizenShowInstanceButton, new Vector3(-8.0f, 135.0f, 0.0f),
+                        out citizenShowUnitButton, new Vector3(-8.0f, 160.0f, 0.0f)
                         );
                     initializedCitizenPanel = true;
                 }
@@ -462,14 +603,14 @@ namespace ModTools
             {
                 InstanceID instance = ReflectionUtil.GetPrivate<InstanceID>(zonedBuildingInfoPanel, "m_InstanceID");
                 var building = BuildingManager.instance.m_buildings.m_buffer[instance.Building];
-                zonedBuildingAssetNameLabel.text = "AssetName: " + building.Info.name;
+                zonedBuildingAssetNameLabel.text = $"AssetName: {building.Info.name}";
             }
 
             if (serviceBuildingInfoPanel.component.isVisible)
             {
                 InstanceID instance = ReflectionUtil.GetPrivate<InstanceID>(serviceBuildingInfoPanel, "m_InstanceID");
                 var building = BuildingManager.instance.m_buildings.m_buffer[instance.Building];
-                serviceBuildingAssetNameLabel.text = "AssetName: " + building.Info.name;
+                serviceBuildingAssetNameLabel.text = $"AssetName: {building.Info.name}";
             }
 
             if (citizenVehicleInfoPanel.component.isVisible)
@@ -479,12 +620,12 @@ namespace ModTools
                 if (instance.Vehicle == 0)
                 {
                     var vehicle = VehicleManager.instance.m_parkedVehicles.m_buffer[instance.ParkedVehicle];
-                    citizenVehicleAssetNameLabel.text = "AssetName: " + vehicle.Info.name;
+                    citizenVehicleAssetNameLabel.text = $"AssetName: {vehicle.Info.name}";
                 }
                 else
                 {
                     var vehicle = VehicleManager.instance.m_vehicles.m_buffer[instance.Vehicle];
-                    citizenVehicleAssetNameLabel.text = "AssetName: " + vehicle.Info.name;
+                    citizenVehicleAssetNameLabel.text = $"AssetName: {vehicle.Info.name}";
                 }
             }
 
@@ -495,12 +636,12 @@ namespace ModTools
                 if (instance.Vehicle == 0)
                 {
                     var vehicle = VehicleManager.instance.m_parkedVehicles.m_buffer[instance.ParkedVehicle];
-                    cityServiceVehicleAssetNameLabel.text = "AssetName: " + vehicle.Info.name;
+                    cityServiceVehicleAssetNameLabel.text = $"AssetName: {vehicle.Info.name}";
                 }
                 else
                 {
                     var vehicle = VehicleManager.instance.m_vehicles.m_buffer[instance.Vehicle];
-                    cityServiceVehicleAssetNameLabel.text = "AssetName: " + vehicle.Info.name;
+                    cityServiceVehicleAssetNameLabel.text = $"AssetName: {vehicle.Info.name}";
                 }
             }
 
@@ -511,12 +652,12 @@ namespace ModTools
                 if (instance.Vehicle == 0)
                 {
                     var vehicle = VehicleManager.instance.m_parkedVehicles.m_buffer[instance.ParkedVehicle];
-                    publicTransportVehicleAssetNameLabel.text = "AssetName: " + vehicle.Info.name;
+                    publicTransportVehicleAssetNameLabel.text = $"AssetName: {vehicle.Info.name}";
                 }
                 else
                 {
                     var vehicle = VehicleManager.instance.m_vehicles.m_buffer[instance.Vehicle];
-                    publicTransportVehicleAssetNameLabel.text = "AssetName: " + vehicle.Info.name;
+                    publicTransportVehicleAssetNameLabel.text = $"AssetName: {vehicle.Info.name}";
                 }
             }
 
@@ -524,7 +665,7 @@ namespace ModTools
             {
                 InstanceID instance = ReflectionUtil.GetPrivate<InstanceID>(animalInfoPanel, "m_InstanceID");
                 var animal = CitizenManager.instance.m_instances.m_buffer[instance.CitizenInstance];
-                animalAssetNameLabel.text = "AssetName: " + animal.Info.name;
+                animalAssetNameLabel.text = $"AssetName: {animal.Info.name}";
             }
 
             if (citizenInfoPanel.component.isVisible)
@@ -533,12 +674,21 @@ namespace ModTools
                 if (instance.Type == InstanceType.CitizenInstance)
                 {
                     var citizen = CitizenManager.instance.m_instances.m_buffer[instance.CitizenInstance];
-                    citizenAssetNameLabel.text = "AssetName: " + citizen.Info.name;
+                    citizenAssetNameLabel.text = $"AssetName: {citizen.Info.name}";
                 }
                 else if (instance.Type == InstanceType.Citizen)
                 {
-                    var citizen = CitizenManager.instance.m_citizens.m_buffer[instance.Citizen];
-                    citizenAssetNameLabel.text = "AssetName: ???";
+                    citizenAssetNameLabel.text = "AssetName: N/A. Instance: N/A";
+                    foreach (var ci in CitizenManager.instance.m_instances.m_buffer)
+                    {
+                        if (ci.m_flags == CitizenInstance.Flags.None || ci.Info == null ||
+                            ci.m_citizen != instance.Citizen)
+                        {
+                            continue;
+                        }
+                        citizenAssetNameLabel.text = $"AssetName: {ci.Info.name}";
+                        break;
+                    }
                 }
             }
         }
