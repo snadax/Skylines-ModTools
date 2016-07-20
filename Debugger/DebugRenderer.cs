37565a69ec86f0826439a991e8e26a495b090da4
Egor Aralov <aei.omsk@gmail.com>
2016-07-21 02:09:32 +0600

    Add Ctrl+G key combo to iterate over UI elements in debug renderer

--- Debugger/DebugRenderer.cs
+++ Debugger/DebugRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using ColossalFramework.UI;
 using UnityEngine;
 
@@ -15,6 +16,8 @@ namespace ModTools
         private GUIStyle infoWindowStyle;
 
         private UIComponent hoveredComponent;
+        private readonly List<UIComponent> hoveredComponents = new List<UIComponent>();
+        private long previousHash = 0;
 
         void Update()
         {
@@ -36,12 +39,22 @@ namespace ModTools
                     {
                         refChain = refChain.Add(current.gameObject);
                         current = current.parent;
-                    };
+                    }
+                    ;
                     refChain = refChain.Add(uiView.gameObject);
 
                     var sceneExplorer = FindObjectOfType<SceneExplorer>();
                     sceneExplorer.ExpandFromRefChain(refChain.Reverse);
                     sceneExplorer.visible = true;
+                }
+                if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.G))
+                {
+                    if (hoveredComponents.Count > 1 && hoveredComponent != null)
+                    {
+                        var index = hoveredComponents.IndexOf(hoveredComponent);
+                        var newIndex = (index + hoveredComponents.Count + 1) % hoveredComponents.Count;
+                        hoveredComponent = hoveredComponents[newIndex];
+                    }
                 }
             }
         }
@@ -75,10 +88,10 @@ namespace ModTools
 
                 infoWindowStyle = new GUIStyle(GUI.skin.box)
                 {
-                    normal = {background = null},
-                    hover = {background = null},
-                    active = {background = null},
-                    focused = {background = null}
+                    normal = { background = null },
+                    hover = { background = null },
+                    active = { background = null },
+                    focused = { background = null }
                 };
             }
 
@@ -95,7 +108,8 @@ namespace ModTools
             var mouse = Input.mousePosition;
             mouse.y = Screen.height - mouse.y;
 
-            hoveredComponent = null;
+            hoveredComponents.Clear();
+            long hash = 0;
             for (int i = components.Length - 1; i > 0; i--)
             {
                 var component = components[i];
@@ -118,13 +132,22 @@ namespace ModTools
                 var position = component.absolutePosition;
                 var size = component.size;
                 var rect = CalculateRealComponentRect(position, size);
-
                 if (rect.Contains(mouse))
                 {
-                    hoveredComponent = component;
-                    break;
+                    hash += CalculateHash(component);
+                    hoveredComponents.Add(component);
                 }
             }
+            if (hoveredComponent != null && hash != previousHash)
+            {
+                hoveredComponent = null;
+                previousHash = hash;
+            }
+            if (hoveredComponent == null && hoveredComponents.Count > 0)
+            {
+                hoveredComponent = hoveredComponents.First();
+            }
+
 
             foreach (var component in components)
             {
@@ -144,7 +167,7 @@ namespace ModTools
             {
                 var coords = mouse;
 
-                var size = new Vector2(300.0f, 220.0f);
+                var size = new Vector2(300.0f, 300.0f);
 
                 if (coords.x + size.x >= Screen.width)
                 {
@@ -173,28 +196,52 @@ namespace ModTools
 
         void DoInfoWindow(int i)
         {
+            if (hoveredComponent == null)
+            {
+                return;
+            }
+            GUI.color = Color.red;
             GUILayout.Label("[Press Ctrl+F to open it in SceneExplorer]");
+            GUI.color = Color.blue;
+            GUILayout.Label("[Press Ctrl+G to iterate]");
+            GUI.color = Color.white;
             GUILayout.Label($"name: {hoveredComponent.name}");
             GUILayout.Label($"type: {hoveredComponent.GetType().Name}");
 
             if (hoveredComponent.parent != null)
             {
-                GUILayout.Label($"parent: {hoveredComponent.parent.name}");
+                GUILayout.Label($"parent: {hoveredComponent.parent?.name}");
             }
 
             GUILayout.Label($"anchor: {hoveredComponent.anchor}");
             GUILayout.Label($"size: {hoveredComponent.size}");
+            GUILayout.Label($"position: {hoveredComponent.position}");
             GUILayout.Label($"relativePosition: {hoveredComponent.relativePosition}");
-            var component = hoveredComponent as UIInteractiveComponent;
-            if (component != null)
+            var interactiveComponent = hoveredComponent as UIInteractiveComponent;
+            if (interactiveComponent != null)
             {
-                GUILayout.Label($"atlas: {component.atlas.name}");                
+                GUILayout.Label($"atlas.name: {interactiveComponent.atlas.name}");
             }
-
-            var hash = HashUtil.HashRect(new Rect(hoveredComponent.relativePosition.x, hoveredComponent.relativePosition.y,
-                   hoveredComponent.size.x, hoveredComponent.size.y));
+            var sprite = hoveredComponent as UISprite;
+            if (sprite != null)
+            {
+                GUILayout.Label($"atlas.name: {sprite.atlas?.name}");
+                GUILayout.Label($"spriteName: {sprite.spriteName}");
+            }
+            var textureSprite = hoveredComponent as UITextureSprite;
+            if (textureSprite != null)
+            {
+                GUILayout.Label($"texture.name: {textureSprite.texture?.name}");
+            }
             GUILayout.Label($"zOrder: {hoveredComponent.zOrder}");
+            var hash = CalculateHash(hoveredComponent);
             GUILayout.Label($"hash: {HashUtil.HashToString(hash)}");
+        }
+
+        private long CalculateHash(UIComponent c)
+        {
+            return HashUtil.HashRect(new Rect(c.relativePosition.x, c.relativePosition.y,
+                c.size.x, c.size.y));
         }
 
         private int RenderSortFunc(UIComponent lhs, UIComponent rhs)
