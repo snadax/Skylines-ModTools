644c7e51e55e998d47abea724c57c5437f58e825
Egor Aralov <aei.omsk@gmail.com>
2017-07-29 22:50:41 +0200

    Improve camera controls

--- Debugger/MeshViewer.cs
+++ Debugger/MeshViewer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using ColossalFramework.UI;
 using ICities;
 using ModTools.Utils;
 using UnityEngine;
@@ -15,9 +16,9 @@ namespace ModTools
 
         private RenderTexture targetRT;
 
-        private float zoom = 100.0f;
-
-        private Quaternion rotation = Quaternion.identity;
+        private float m_Distance = 4f;
+        private Vector2 m_PreviewDir = new Vector2(120f, -20f);
+        private Bounds bounds;
         private Vector2 lastMousePos = Vector2.zero;
 
         private Camera meshViewerCamera;
@@ -32,6 +33,15 @@ namespace ModTools
             : base("Mesh Viewer", new Rect(512, 128, 512, 512), skin)
         {
             onDraw = DrawWindow;
+            onClose = () =>
+            {
+                Destroy(this);
+            };
+            onUnityDestroy = () =>
+            {
+                Destroy(meshViewerCamera);
+                Destroy(targetRT);
+            };
 
             try
             {
@@ -44,7 +54,7 @@ namespace ModTools
 
             meshViewerCamera = gameObject.AddComponent<Camera>();
             meshViewerCamera.transform.position = new Vector3(-10000.0f, -10000.0f, -10000.0f);
-            meshViewerCamera.fieldOfView = 20.0f;
+            meshViewerCamera.fieldOfView = 30f;
             meshViewerCamera.backgroundColor = Color.grey;
             meshViewerCamera.nearClipPlane = 1.0f;
             meshViewerCamera.farClipPlane = 1000.0f;
@@ -69,6 +79,7 @@ namespace ModTools
             {
                 meshViewer.previewMaterial.mainTexture = material.mainTexture;
             }
+            meshViewer.Setup();
             meshViewer.visible = true;
             return meshViewer;
         }
@@ -94,9 +105,15 @@ namespace ModTools
                 light.enabled = true;
             }
 
-            var pos = meshViewerCamera.transform.position + new Vector3(0.0f, -zoom, -zoom);
-            var trs = Matrix4x4.TRS(pos, rotation, new Vector3(1.0f, 1.0f, 1.0f));
-            meshViewerCamera.transform.LookAt(pos, Vector3.up);
+            float magnitude = bounds.extents.magnitude;
+            float num1 = magnitude + 16f;
+            float num2 = magnitude * this.m_Distance;
+            this.meshViewerCamera.transform.position = -Vector3.forward * num2;
+            this.meshViewerCamera.transform.rotation = Quaternion.identity;
+            this.meshViewerCamera.nearClipPlane = Mathf.Max(num2 - num1 * 1.5f, 0.01f);
+            this.meshViewerCamera.farClipPlane = num2 + num1 * 1.5f;
+            Quaternion q = Quaternion.Euler(this.m_PreviewDir.y, 0.0f, 0.0f) * Quaternion.Euler(0.0f, this.m_PreviewDir.x, 0.0f);
+            var trs = Matrix4x4.TRS(q * -bounds.center, q, Vector3.one);
 
             var material1 = (useOriginalShader && material != null) ? material : previewMaterial;
             Graphics.DrawMesh(previewMesh, trs, material1, 0, meshViewerCamera, 0, null, false, false);
@@ -175,9 +192,10 @@ namespace ModTools
                     var pos = Event.current.mousePosition;
                     if (lastMousePos != Vector2.zero)
                     {
-                        var delta = pos - lastMousePos;
-                        zoom += delta.y;
-                        rotation *= Quaternion.Euler(0.0f, -delta.x, 0.0f);
+                        Vector2 moveDelta = pos - lastMousePos;
+                        moveDelta.y = -moveDelta.y;
+                        this.m_PreviewDir -= moveDelta / Mathf.Min(this.targetRT.width, this.targetRT.height) * UIView.GetAView().ratio * 140f;
+                        this.m_PreviewDir.y = Mathf.Clamp(this.m_PreviewDir.y, -90f, 90f);
                     }
                     lastMousePos = pos;
                 }
@@ -189,6 +207,16 @@ namespace ModTools
                 title = "Mesh Viewer";
                 GUILayout.Label("Use the Scene Explorer to select a Mesh for preview");
             }
+        }
+
+        public void Setup()
+        {
+            this.bounds = new Bounds(Vector3.zero, Vector3.zero);
+            if (!((UnityEngine.Object)this.previewMesh != (UnityEngine.Object)null))
+                return;
+            foreach (Vector3 vertex in this.previewMesh.vertices)
+                this.bounds.Encapsulate(vertex);
+            this.m_Distance = 4f;
         }
     }
 }
