c19e75f7846a1b16407a32b90acdc16d59b43452
dymanoid <9433345+dymanoid@users.noreply.github.com>
2019-06-14 20:16:13 +0200

    Change the notification color in the console

--- Debugger/Console.cs
+++ Debugger/Console.cs
@@ -387,7 +387,7 @@ namespace ModTools
             {
                 GUILayout.BeginHorizontal(Skin.box);
 
-                GUI.contentColor = Color.blue;
+                GUI.contentColor = Color.cyan;
                 GUILayout.Label(item.Value);
                 GUI.contentColor = Color.white;
 
