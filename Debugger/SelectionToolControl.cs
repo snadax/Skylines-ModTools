c9b00606f9a537f75066fa56f0d2b8f8cc3404a5
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2019-07-19 02:40:07 +0200

    Fix selection tool bugs

--- Debugger/SelectionToolControl.cs
+++ Debugger/SelectionToolControl.cs
@@ -26,23 +26,16 @@ namespace ModTools
             var textureBar = AtlasUtil.LoadTextureFromAssembly("ModTools.SelectionToolBar.png");
             textureBar.name = "SelectionToolBar";
 
-            var mode = ToolManager.instance.m_properties.m_mode;
-            var baseSpriteName =
-                mode == ItemClass.Availability.AssetEditor
-                    ? "InfoIconBase"
-                    : mode == ItemClass.Availability.Game
-                        ? "RoundBackBig"
-                        : "OptionBase";
+            var escButton = (UIButton)UIView.Find("Esc");
             var atlas = AtlasUtil.CreateAtlas(new[]
-            {
-                textureButton,
-                textureBar,
-                GetTextureByName($"{baseSpriteName}Disabled"),
-                GetTextureByName($"{baseSpriteName}Hovered"),
-                GetTextureByName($"{baseSpriteName}Pressed"),
-                GetTextureByName(
-                    mode == ItemClass.Availability.AssetEditor ? $"{baseSpriteName}Normal" : baseSpriteName),
-            });
+                {
+                    textureButton,
+                    textureBar,
+                    GetTextureByName(escButton.disabledBgSprite, escButton.atlas),
+                    GetTextureByName(escButton.hoveredBgSprite, escButton.atlas),
+                    GetTextureByName(escButton.pressedBgSprite, escButton.atlas),
+                    GetTextureByName(escButton.normalBgSprite, escButton.atlas),
+                });
             var buttonGo = new GameObject("SelectionToolButton");
             m_Button = buttonGo.AddComponent<UIButton>();
             m_Button.tooltip = "Mod Tools - Selection Tool";
@@ -51,29 +44,28 @@ namespace ModTools
             m_Button.pressedFgSprite = "SelectionToolButton";
             m_Button.disabledFgSprite = "SelectionToolButton";
             m_Button.focusedFgSprite = "SelectionToolButton";
-            m_Button.normalBgSprite =
-                mode == ItemClass.Availability.AssetEditor ? $"{baseSpriteName}Normal" : baseSpriteName;
-            m_Button.focusedBgSprite =
-                mode == ItemClass.Availability.AssetEditor ? $"{baseSpriteName}Normal" : baseSpriteName;
-            m_Button.hoveredBgSprite = $"{baseSpriteName}Hovered";
-            m_Button.pressedBgSprite = $"{baseSpriteName}Pressed";
-            m_Button.disabledBgSprite = $"{baseSpriteName}Disabled";
+            m_Button.normalBgSprite = escButton.normalBgSprite;
+            m_Button.focusedBgSprite = escButton.normalBgSprite;
+            m_Button.hoveredBgSprite = escButton.hoveredBgSprite;
+            m_Button.pressedBgSprite = escButton.pressedBgSprite;
+            m_Button.disabledBgSprite = escButton.disabledBgSprite;
             m_Button.playAudioEvents = true;
             m_Button.width = 46f;
             m_Button.height = 46f;
             UIView.GetAView().AttachUIComponent(buttonGo);
-            m_Button.absolutePosition = new Vector3(1700, 10); // TODO: make dynamic
+            m_Button.absolutePosition = escButton.absolutePosition - new Vector3(95, 0, 0);
             m_Button.atlas = atlas;
             m_Button.eventClicked += (c, e) => ToggleTool();
+            m_Button.isVisible = MainWindow.Instance.Config.SelectionTool;
 
             var dragGo = new GameObject("SelectionToolDragHandler");
-            dragGo.transform.parent = transform;
+            dragGo.transform.parent = m_Button.transform;
             dragGo.transform.localPosition = Vector3.zero;
             var drag = dragGo.AddComponent<UIDragHandle>();
             drag.tooltip = m_Button.tooltip;
             drag.width = m_Button.width;
             drag.height = m_Button.height;
-            
+
             var barGo = new GameObject("SelectionToolBar");
             m_Bar = barGo.AddComponent<UITiledSprite>();
             UIView.GetAView().AttachUIComponent(barGo);
@@ -103,8 +95,20 @@ namespace ModTools
                 m_Bar.Hide();
             }
 
-            if (!MainWindow.Instance.Config.SelectionTool)
+            if (MainWindow.Instance.Config.SelectionTool)
             {
+                if (!m_Button.isVisible)
+                {
+                    m_Button.Show();
+                }
+            }
+            else
+            {
+                if (m_Button.isVisible)
+                {
+                    m_Button.Hide();
+                }
+
                 return;
             }
 
@@ -127,12 +131,12 @@ namespace ModTools
 
             if (tool.enabled)
             {
-                ValueAnimator.Animate("BulldozerBar", (Action<float>) (val =>
+                ValueAnimator.Animate("BulldozerBar", val =>
                     {
                         Vector3 relativePosition = this.m_Bar.relativePosition;
                         relativePosition.y = val;
                         this.m_Bar.relativePosition = relativePosition;
-                    }),
+                    },
                     new AnimatedFloat(
                         this.m_FullscreenContainer.relativePosition.y + this.m_FullscreenContainer.size.y -
                         this.m_Bar.size.y,
@@ -146,21 +150,21 @@ namespace ModTools
                 ToolsModifierControl.mainToolbar.CloseEverything();
                 ToolsModifierControl.SetTool<SelectionTool>();
                 this.m_Bar.Show();
-                ValueAnimator.Animate("BulldozerBar", (Action<float>) (val =>
+                ValueAnimator.Animate("BulldozerBar", val =>
                     {
                         Vector3 relativePosition = this.m_Bar.relativePosition;
                         relativePosition.y = val;
                         this.m_Bar.relativePosition = relativePosition;
-                    }),
+                    },
                     new AnimatedFloat(this.m_FullscreenContainer.relativePosition.y + this.m_FullscreenContainer.size.y,
                         this.m_FullscreenContainer.relativePosition.y + this.m_FullscreenContainer.size.y -
                         this.m_Bar.size.y, 0.3f));
             }
         }
 
-        private static Texture2D GetTextureByName(string name)
+        private static Texture2D GetTextureByName(string name, UITextureAtlas atlas)
         {
-            return UIView.GetAView().defaultAtlas.sprites.Find(sprite => sprite.name == name).texture;
+            return atlas.sprites.Find(sprite => sprite.name == name).texture;
         }
     }
 }
